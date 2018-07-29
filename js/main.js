ec64ca5d68438051f36c87c5eb90ec855daf34e1
Joao Fonseca <30560284+FonsecaJoao@users.noreply.github.com>
2018-07-30 00:39:15 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,22 +1,44 @@
 'use strict';
-
+let george, gregory, tiger, sponge, felix;
 let moves = 0;
 let money = document.querySelector('#money');
+const catName = document.querySelector('#cat-name');
 
 $('#cat').click(function(e) {
 	incrementMoney();
 });
 
-$('#cattwo').click(function(e) {
-	incrementMoney();
-});
-
-// $('#cat').tap(function(e) {
-// 	incrementMoney();
-// })
-
 function incrementMoney() {
 	moves++;
 	money.innerHTML = 'Money: ' + moves;
 }
-  
+
+$('#george').click(function(e) {
+	$('#cat').attr('src', 'img/george.png');
+	catName.innerHTML = 'george';
+	george = moves;
+});
+
+$('#gregory').click(function(e) {
+	$('#cat').attr('src', 'img/gregory.png');
+	catName.innerHTML = 'gregory';
+	gregory = moves;
+});
+
+$('#tiger').click(function(e) {
+	$('#cat').attr('src', 'img/tiger.png');
+	catName.innerHTML = 'tiger';
+	tiger = moves;
+});
+
+$('#sponge').click(function(e) {
+	$('#cat').attr('src', 'img/sponge.png');
+	catName.innerHTML = 'sponge';
+	sponge = moves;
+});
+
+$('#felix').click(function(e) {
+	$('#cat').attr('src', 'img/felix.png');
+	catName.innerHTML = 'felix';
+	felix = moves;
+});
