5abaf949b5efdcacce38ba03dcfb23faf87f59b9
Joao Fonseca <30560284+FonsecaJoao@users.noreply.github.com>
2018-07-30 15:05:59 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,44 +1,78 @@
 'use strict';
 let george, gregory, tiger, sponge, felix;
-let moves = 0;
 let money = document.querySelector('#money');
 const catName = document.querySelector('#cat-name');
+let catMone = 'george';
+let catMoney = {
+	george: 0,
+	gregory: 0,
+	tiger: 0,
+	sponge: 0,
+	felix: 0
+};
 
 $('#cat').click(function(e) {
 	incrementMoney();
 });
 
 function incrementMoney() {
-	moves++;
-	money.innerHTML = 'Money: ' + moves;
+	switch (catMone) {
+		case 'george':
+			catMoney.george++;
+			money.innerHTML = 'Money: ' + catMoney.george;
+			break;
+		case 'gregory':
+			catMoney.gregory++;
+			money.innerHTML = 'Money: ' + catMoney.gregory;
+			break;
+		case 'tiger':
+			catMoney.tiger++;
+			money.innerHTML = 'Money: ' + catMoney.tiger;
+			break;
+		case 'sponge':
+			catMoney.sponge++;
+			money.innerHTML = 'Money: ' + catMoney.sponge;
+			break;
+		case 'felix':
+			catMoney.felix++;
+			money.innerHTML = 'Money: ' + catMoney.felix;
+			break;
+
+	}
 }
 
 $('#george').click(function(e) {
+	money.innerHTML = 'Money: ' + catMoney.george;
 	$('#cat').attr('src', 'img/george.png');
 	catName.innerHTML = 'george';
-	george = moves;
+	catMone = 'george';
+	
 });
 
 $('#gregory').click(function(e) {
+	money.innerHTML = 'Money: ' + catMoney.gregory;
 	$('#cat').attr('src', 'img/gregory.png');
 	catName.innerHTML = 'gregory';
-	gregory = moves;
+	catMone = 'gregory';
 });
 
 $('#tiger').click(function(e) {
+	money.innerHTML = 'Money: ' + catMoney.tiger;
 	$('#cat').attr('src', 'img/tiger.png');
 	catName.innerHTML = 'tiger';
-	tiger = moves;
+	catMone = 'tiger';
 });
 
 $('#sponge').click(function(e) {
+	money.innerHTML = 'Money: ' + catMoney.sponge;
 	$('#cat').attr('src', 'img/sponge.png');
 	catName.innerHTML = 'sponge';
-	sponge = moves;
+	catMone = 'sponge';
 });
 
 $('#felix').click(function(e) {
+	money.innerHTML = 'Money: ' + catMoney.felix;
 	$('#cat').attr('src', 'img/felix.png');
 	catName.innerHTML = 'felix';
-	felix = moves;
+	catMone = 'felix';
 });
